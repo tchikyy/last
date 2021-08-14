8009b2e006c33ddc84ae6b1dfe8b944d7c06db61
tchikyy <76624013+tchikyy@users.noreply.github.com>
2021-08-14 03:49:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads/'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/", methods=["GET", "POST"])
 def predict():
@@ -21,31 +24,20 @@ def predict():
         file = request.files["file"]
         if not file:
             return
-
-        img_bytes = file.read()
-        img = Image.open(io.BytesIO(img_bytes))
-        results = model(img, size=640)
-
-        # for debugging
-        # data = results.pandas().xyxy[0].to_json(orient="records")
-        # return data
-
-        results.render()  # updates results.imgs with boxes and labels
-        for img in results.imgs:
-            img_base64 = Image.fromarray(img)
-            img_base64.save("static/image0.jpg", format="JPEG")
-        return redirect("static/image0.jpg")
+            
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+    return render_template('index.html', filename=filename)
+    
+ 
+@app.route('/display/<filename>')
+def display_image(filename):
+    #print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
     return render_template("index.html")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
-    args = parser.parse_args()
-
-    model = torch.hub.load(
-        "ultralytics/yolov5", "yolov5s", pretrained=True, force_reload=True
-    ).autoshape()  # force_reload = recache latest code
-    model.eval()
     app.run(debug=True)  # debug=True causes Restarting with stat
